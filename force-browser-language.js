88a387186a5efb5ad49c80cbde3bbf371de0b25d
Alexander Lopatin <alopatindev@codonaft.com>
2025-09-19 20:03:49 +0400

    Add force-browser-language.js

---
+++ force-browser-language.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name Force Browser Language
+// @version 0.1
+// @downloadURL https://userscripts.codonaft.com/force-browser-language.js
+// @match https://*.google.com/*
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+   const lang = navigator.language || 'en';
+   const url = new URL(window.location.href);
+   if (url.searchParams.get('hl') !== lang) {
+     window.stop();
+     url.searchParams.set('hl', lang);
+     window.location.href = url.toString();
+   }
+})()
