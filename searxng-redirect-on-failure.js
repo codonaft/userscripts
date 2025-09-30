560ad0adc9617875f5e5e417b5d73d4a6743d9c6
Alexander Lopatin <alopatindev@codonaft.com>
2025-10-01 00:12:54 +0400

    Update

--- searxng-redirect-on-failure.js
+++ searxng-redirect-on-failure.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name SearXNG Redirect On Failure
 // @description Redirect to a random SearXNG instance in case of error and empty result
-// @version 0.3
+// @version 0.4
 // @downloadURL https://userscripts.codonaft.com/searxng-redirect-on-failure.js
 // ==/UserScript==
 
@@ -9,21 +9,24 @@
   'use strict';
 
   if (performance.getEntriesByType('navigation')[0]?.responseStatus !== 200) return;
-  if (!document.head.querySelector('link[type="application/opensearchdescription+xml"]')?.title?.includes('SearXNG') && !document.body.querySelector('a[href="https://searx.space"]')?.textContent?.includes('Public instances')) return;
+  const b = document.body;
+  if (!document.head.querySelector('link[type="application/opensearchdescription+xml"]')?.title?.includes('SearXNG') && !b.querySelector('a[href="https://searx.space"]')?.textContent?.includes('Public instances')) return;
 
-  if (!document.body.querySelector('td.response-time') && document.body.querySelector('td.response-error')) {
-    const url = new URL(window.location.href);
-    if (url.host.endsWith('.onion')) {
-      url.host = 'codonaftbvv4j5k7nsrdivbdblycqrng5ls2qkng6lm77svepqjyxgid.onion';
-    } else if (url.host.endsWith('.i2p')) {
-      url.host = 'codonftbnpdkjwyflssto3iklawhuthbe37l6swigegqkyyfmiqa.b32.i2p';
-    } else {
-      url.host = 'codonaft.com';
-    }
-    url.pathname = '/searxng';
-    url.searchParams.set('fast', '0');
-    const params = url.searchParams.toString();
-    url.searchParams.forEach((_, i) => url.searchParams.delete(i));
-    window.location.replace(`${url.toString()}#${params}`);
+  const hasResults = (b.querySelector('td.response-time') || !b.querySelector('td.response-error')) && !b.querySelector('div.dialog-error-block')?.innerText.includes('No results were found');
+  if (hasResults) return;
+
+  console.log('SearXNG: no results');
+  const url = new URL(window.location.href);
+  if (url.host.endsWith('.onion')) {
+    url.host = 'codonaftbvv4j5k7nsrdivbdblycqrng5ls2qkng6lm77svepqjyxgid.onion';
+  } else if (url.host.endsWith('.i2p')) {
+    url.host = 'codonftbnpdkjwyflssto3iklawhuthbe37l6swigegqkyyfmiqa.b32.i2p';
+  } else {
+    url.host = 'codonaft.com';
   }
+  url.pathname = '/searxng';
+  url.searchParams.set('fast', '0');
+  const params = url.searchParams.toString();
+  url.searchParams.forEach((_, i) => url.searchParams.delete(i));
+  window.location.replace(`${url.toString()}#${params}`);
 })()
