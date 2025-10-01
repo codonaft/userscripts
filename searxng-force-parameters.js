9c57440660259b401c205a27843ea99e2e1d76b2
Alexander Lopatin <alopatindev@codonaft.com>
2025-10-01 06:27:11 +0400

    Add searxng-force-parameters.js

---
+++ searxng-force-parameters.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name Force SearXNG Parameters
+// @version 0.1
+// @downloadURL https://userscripts.codonaft.com/searxng-force-parameters.js
+// ==/UserScript==
+
+(_ => {
+  'use strict';
+
+  if (performance.getEntriesByType('navigation')[0]?.responseStatus !== 200) return;
+  if (!document.head.querySelector('link[type="application/opensearchdescription+xml"]')?.title?.includes('SearXNG') && !document.body.querySelector('a[href="https://searx.space"]')?.textContent?.includes('Public instances')) return;
+
+  const form = document.body.querySelector('form#search');
+  if (!form) return;
+
+  // https://docs.searxng.org/dev/search_api.html
+  const params = {
+    'image_proxy': 'True',
+    'safesearch': '0',
+  };
+
+  Object.entries(params).forEach(([k, v]) => {
+    const input = document.createElement('input');
+    input.type = 'hidden';
+    input.name = k;
+    input.value = v;
+    form.appendChild(input);
+  });
+})()
