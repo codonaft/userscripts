92321c35bfcef59834fb7ae966ff147f7f3f4ea8
Alexander Lopatin <alopatindev@codonaft.com>
2025-10-10 01:34:11 +0400

    Update

--- force-searxng-parameters.user.js
+++ force-searxng-parameters.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Force SearXNG Parameters
 // @icon https://www.google.com/s2/favicons?sz=64&domain=searx.space
-// @version 0.4
+// @version 0.5
 // @downloadURL https://userscripts.codonaft.com/force-searxng-parameters.user.js
 // ==/UserScript==
 
@@ -13,24 +13,53 @@
   if (!b) return;
   if (!document.head?.querySelector('link[type="application/opensearchdescription+xml"]')?.title?.toLowerCase().includes('searx') && ![...b.querySelectorAll('a[href="https://searx.space"]')].find(i => i.textContent?.includes('Public instances'))) return;
 
+  const disabledEngines = {
+    'general': ['360search', 'baidu', 'bing', 'bpb', 'quark', 'sogou', 'tagesschau', 'wikimini'],
+  };
+
+  const enabledEngines = {
+    'files': ['piratebay', 'solidtorrents', 'z-library'],
+    'general': ['duckduckgo', 'google', 'mozhi', 'openlibrary', 'wikipedia', 'wolframalpha'],
+    'images': ['brave.images', 'duckduckgo images', 'google images', 'mojeek images', 'presearch images', 'qwant images', 'startpage images'],
+    'it': ['askubuntu', 'bitbucket', 'codeberg', 'crates.io', 'docker hub', 'gitea.com', 'gitlab', 'habrahabr', 'hackernews', 'lib.rs', 'lobste.rs', 'sourcehut', 'stackoverflow', 'superuser'],
+    'music': ['bandcamp', 'genius', 'mixcloud', 'radio browser', 'soundcloud', 'youtube'],
+    'other': ['github code', 'erowid', 'duckduckgo weather', 'goodreads'],
+    'science': ['arxiv', 'google scholar', 'pubmed', 'semantic scholar'],
+    'social media': ['reddit'],
+    'videos': ['odysee', 'peertube','sepiasearch', 'vimeo', 'youtube'],
+    'onions': ['ahmia', 'torch'],
+  };
+
+  const enabledPlugins = ['calculator', 'oa_doi_rewrite', 'tracker_url_remover', 'Vim-like_hotkeys'];
+
   // https://docs.searxng.org/dev/search_api.html
   const params = {
-    'categories': 'general,images,videos,news,it,science,files,social_media',
     'autocomplete': '',
+    'categories': ['general', 'it'].join(','),
+    'disabled_engines': Object.values(disabledEngines).flatMap(i => i).join(','),
+    'enabled_engines': Object.values(enabledEngines).flatMap(i => i).filter(i => !Object.values(disabledEngines).flatMap(i => i).includes(i)).join(','),
+    'enabled_plugins': enabledPlugins.join(','),
     'image_proxy': 'True',
     'safesearch': 0,
   };
 
   const cookies = {
-    'autocomplete': params.autocomplete,
-    'enabled_plugins': 'calculator',
+    'advanced_search': 1,
+    'autocomplete': params.autocomplete || '',
+    'categories': params.categories || 'general',
+    'disabled_engines': 'bing__general',
+    'doi_resolver': 'sci-hub.se',
+    'enabled_engines': Object.entries(enabledEngines).flatMap(([category, v]) => v.map(engine => `${engine}__${category}`)).join(','),
+    'enabled_plugins': enabledPlugins.join(','),
     'favicon_resolver': '',
     'hotkeys': 'vim',
     'image_proxy': +(params.image_proxy === 'True'),
     'infinite_scroll': 1,
     'locale': 'en',
     'method': 'POST',
+    'query_in_title': 0,
     'safesearch': params.safesearch || 0,
+    'search_on_category_select': 1,
   };
 
   const form = b.querySelector('form#search');
@@ -44,5 +73,11 @@
     });
   }
 
-  Object.entries(params).forEach(([k, v]) => document.cookie = `${k}=${v}`);
+  Object.entries(cookies).forEach(([k, v]) => document.cookie = `${k}=${v}`);
+
+  if (!params.autocomplete) {
+    const style = document.createElement('style');
+    style.innerHTML = 'div.autocomplete { display: none !important }';
+    b.appendChild(style);
+  }
 })();
