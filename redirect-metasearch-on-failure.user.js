197f2b52291f5502108d11a0675a34089cac561f
Alexander Lopatin <alopatindev@codonaft.com>
2025-10-16 00:16:19 +0400

    Update

--- redirect-metasearch-on-failure.user.js
+++ redirect-metasearch-on-failure.user.js
@@ -1,8 +1,7 @@
 // ==UserScript==
 // @name Fallback for the cute metasearch engine https://github.com/mat-1/metasearch2
 // @description Uses Minimalist SearXNG Redirector on failure
-// @icon https://external-content.duckduckgo.com/ip3/s.matdoes.dev.ico
-// @version 0.1
+// @version 0.2
 // @downloadURL https://userscripts.codonaft.com/redirect-metasearch-on-failure.user.js
 // ==/UserScript==
 
@@ -13,7 +12,7 @@ if (performance.getEntriesByType('navigation')[0]?.responseStatus !== 200) retur
 if (document.head?.querySelector('link[type="application/opensearchdescription+xml"]')?.title !== 'metasearch') return;
 
 const q = new URLSearchParams(window.location.search).get('q');
-if (q && !document.querySelector('div.search-result')) {
+if (q && !document.querySelector('div.search-result, div.image-result')) {
   const params = new URLSearchParams({ q });
   window.location.replace(`https://codonaft.com/searxng#${params}`);
 }
