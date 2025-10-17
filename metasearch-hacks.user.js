b3b632730ca1cc95e39757f4b970f4a531e4036d
Alexander Lopatin <alopatindev@codonaft.com>
2025-10-17 21:09:47 +0400

    Update

--- metasearch-hacks.user.js
+++ metasearch-hacks.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Hacks for the cute metasearch engine https://github.com/mat-1/metasearch2
-// @version 0.4
+// @version 0.5
 // @downloadURL https://userscripts.codonaft.com/metasearch-hacks.user.js
 // ==/UserScript==
 
@@ -10,8 +10,9 @@
 if (performance.getEntriesByType('navigation')[0]?.responseStatus !== 200) return;
 if (document.head?.querySelector('link[type="application/opensearchdescription+xml"]')?.title !== 'metasearch') return;
 
-const REDIRECT_ON_FAILURE = true;
 const FIX_IMAGES = true;
+const REDIRECT_ON_FAILURE = true;
+const SHOW_LOG_ON_SEARCH_HOVER = true;
 
 const body = document.body;
 const params = new URLSearchParams(window.location.search);
@@ -25,23 +26,35 @@ if (REDIRECT_ON_FAILURE && q && imageResults.length === 0 && !body.querySelector
   window.location.replace(`https://codonaft.com/searxng#${newParams}`);
 }
 
-if (FIX_IMAGES) {
-  const HIDE = '__hide';
-  const LARGE = '__large';
-  try {
-    const style = document.createElement('style');
-    style.innerHTML = `
-      .${HIDE} { display: none !important }
-      .${LARGE} {
-        height: 30rem !important;
-        object-fit: contain !important;
-      }
-    `;
-    body.appendChild(style);
-  } catch (e) {
-    console.error(e);
-  }
+const DARK = '__dark';
+const HIDE = '__hide';
+const LARGE = '__large';
+try {
+  const style = document.createElement('style');
+  style.innerHTML = `
+    .${DARK} {
+      display: block !important;
+      opacity: 0.18;
+    }
+    .${HIDE} { display: none !important }
+    .${LARGE} {
+      height: 30rem !important;
+      object-fit: contain !important;
+    }
+  `;
+  body.appendChild(style);
 
+  if (SHOW_LOG_ON_SEARCH_HOVER) {
+    const searchButton = body.querySelector('input[type="submit"][value="Search"]');
+    const progressUpdates = body.querySelector('div.progress-updates');
+    searchButton?.addEventListener('mouseenter', _ => progressUpdates?.classList.add(DARK));
+    searchButton?.addEventListener('mouseleave', _ => progressUpdates?.classList.remove(DARK));
+  }
+} catch (e) {
+  console.error(e);
+}
+
+if (FIX_IMAGES) {
   let scrollX;
   let scrollY;
   let largeImage;
